2a4a466c7b2cf3558ea2170c3b90c25f58f363ce
Hariom <hariomsahuu2005@gmail.com>
2026-02-11 10:14:14 +0530

    working on homePage

--- app/components/(homePage)/homePage.tsx
+++ app/components/(homePage)/homePage.tsx
@@ -1,11 +1,17 @@
 import NavBar from "@/app/components/navBar";
 import HeroSection from "./heroSection";
+import MaterialExcellence from "./materialExcellence";
+import MaterialSignature from "./materialSignaturet";
+import MaterialDensity from "./materialDensity";
 
 const HomePage = () => {
   return (
     <>
       <NavBar />
       <HeroSection />
+      {/* <MaterialExcellence /> */}
+      {/* <MaterialSignature /> */}
+      <MaterialDensity />
     </>
   );
 };
