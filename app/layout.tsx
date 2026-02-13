9f74f4b838f2689cf3b5af29129d063a9c59f1c4
Hariom <hariomsahuu2005@gmail.com>
2026-02-13 11:41:53 +0530

    updated favicon for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,17 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/navBar";
 import Footer from "./components/footer";
+import favicon from "@/public/favicon.ico";
 export const metadata: Metadata = {
   title: "SGOI | Home",
   description: "Aluminium Foil / Butter Paper Jumbo Rolls Manufacturer",
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      {
+        url: "/favicon.ico",
+        href: "/favicon.ico",
+      },
+    ],
   },
 };
 
