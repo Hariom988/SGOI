5922852df28d84cb03695bb3a12208a6539e54ec
Hariom <hariomsahuu2005@gmail.com>
2026-02-23 17:29:39 +0530

    update the app's routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
-<<<<<<< HEAD
-import Navbar from "./components/navBar";
-import Footer from "./components/footer";
-import favicon from "@/public/favicon.ico";
-=======
-
->>>>>>> dashboard
 export const metadata: Metadata = {
   title: "SGOI | Home",
   description: "Aluminium Foil / Butter Paper Jumbo Rolls Manufacturer",
   icons: {
-<<<<<<< HEAD
-    icon: [
-      {
-        url: "/favicon.ico",
-        href: "/favicon.ico",
-      },
-    ],
-=======
     icon: [{ url: "/favicon.ico", href: "/favicon.ico" }],
->>>>>>> dashboard
   },
 };
 
@@ -29,15 +13,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-<<<<<<< HEAD
-      <body>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-=======
       <body>{children}</body>
->>>>>>> dashboard
     </html>
   );
 }
