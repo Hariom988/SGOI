8b229e6b17401211f4aab38f5b5b8dac9f278cef
Hariom <hariomsahuu2005@gmail.com>
2026-02-24 17:37:23 +0530

    replaced SGOI with SPl

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 export const metadata: Metadata = {
-  title: "SGOI | Home",
+  title: "SPL | Home",
   description: "Aluminium Foil / Butter Paper Jumbo Rolls Manufacturer",
   icons: {
     icon: [{ url: "/favicon.ico", href: "/favicon.ico" }],
